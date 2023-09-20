a8e6c072bd1a0e9be933e40bb1e4aba04c4568ff
oleovini <leonardo.costa3@estudante.ifms.edu.br>
2023-09-20 18:11:01 -0400

    connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,13 @@ const { Sequelize } = require('sequelize');
 const config = require('../config/config')
 require('dotenv').config();
 
-const sequelize = new Sequelize(    );
+const sequelize = new Sequelize(config.development);//<---- Puxando a variavel config que guarda o arquivo config.js que guarda as informações do banco de dados
 
 try {
   sequelize.authenticate();
-  console.log('              ');
+  console.log('Sucesso ao criar Banco de Dados');
 } catch (error) {
-  console.error('             ', error);
+  console.error('Erro ao criar Banco de Dados', error);
 }
 
 module.exports = { Sequelize, sequelize };
